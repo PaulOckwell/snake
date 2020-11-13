47931ffe341c132798e1451ac4fcce939e2c5dcb
Paul Ockwell <43760695+PaulOckwell@users.noreply.github.com>
2020-11-13 00:31:15 +0000

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,109 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const box = 25;
+const canvasSize = 23;
+
+//score variable
+let score = 0;
+
+//load snake starting position
+let snake = [];
+
+snake[0] = {
+	x: Math.floor(canvasSize / 2) * box,
+	y: Math.floor(canvasSize / 2) * box,
+};
+
+//set direction getting pressed by arrow keys
+let dir;
+var pressed = false;
+document.addEventListener('keydown', direction);
+
+function direction(event) {
+	if (event.keyCode == 37 && dir != 'RIGHT') {
+		dir = 'LEFT';
+	} else if (event.keyCode == 38 && dir != 'DOWN') {
+		dir = 'UP';
+	} else if (event.keyCode == 39 && dir != 'LEFT') {
+		dir = 'RIGHT';
+	} else if (event.keyCode == 40 && dir != 'UP') {
+		dir = 'DOWN';
+	}
+}
+
+// set the location of our food
+let food = {
+	x: Math.floor(1 + Math.random() * (canvasSize - 1)) * box,
+	y: Math.floor(1 + Math.random() * (canvasSize - 1)) * box,
+};
+
+//draw function
+function draw() {
+	//draw the background
+	ctx.fillStyle = 'lightblue';
+	ctx.fillRect(box, box, canvasSize * box - box, canvasSize * box - box);
+
+	//draw the snake head and tail
+	for (let i = 0; i < snake.length; i++) {
+		ctx.fillStyle = 'green';
+		ctx.fillRect(snake[i].x, snake[i].y, box, box);
+		if (snake[i].x == food.x && snake[i].y == food.y) {
+			food = {
+				x: Math.floor(1 + Math.random() * (canvasSize - 1)) * box,
+				y: Math.floor(1 + Math.random() * (canvasSize - 1)) * box,
+			};
+		}
+	}
+
+	//move the snake head
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	if (dir == 'LEFT') snakeX -= box;
+	if (dir == 'RIGHT') snakeX += box;
+	if (dir == 'UP') snakeY -= box;
+	if (dir == 'DOWN') snakeY += box;
+
+	// if the snake eats the food
+	if (snakeX == food.x && snakeY == food.y) {
+		score += 1;
+		food = {
+			x: Math.floor(1 + Math.random() * (canvasSize - 1)) * box,
+			y: Math.floor(1 + Math.random() * (canvasSize - 1)) * box,
+		};
+	} else {
+		snake.pop();
+	}
+
+	let newHead = {
+		x: snakeX,
+		y: snakeY,
+	};
+
+	//check collision
+	function collision(head, array) {
+		for (let i = 0; i < array.length; i++) {
+			if (head.x == array[i].x && head.y == array[i].y) {
+				return true;
+			}
+		}
+		return false;
+	}
+	//game over
+	if (snakeX < box || snakeY < box || snakeX > (canvasSize - 1) * box || snakeY > (canvasSize - 1) * box || collision(newHead, snake)) {
+		clearInterval(game);
+	}
+
+	snake.unshift(newHead);
+	//draw in food
+	ctx.fillStyle = 'red';
+	ctx.fillRect(food.x, food.y, box, box);
+
+	//draw score
+	ctx.fillStyle = 'white';
+	ctx.font = '24px Changa one';
+	ctx.clearRect(0, 0, 50, 25);
+	ctx.fillText(score, box, 0.8 * box);
+}
+
+let game = setInterval(draw, 100);
